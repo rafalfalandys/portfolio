1cf11fcb2bf63371e0f69364eb722d25548633ec
rafalfalandys <rafalfalandys@gmail.com>
2023-01-14 16:29:05 +0100

    browser router changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // hasrouter instead of browser router because it works with netlify
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
