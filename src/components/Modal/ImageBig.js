95a6fe7183f72c7b7a8ee7692af586f327cf0a1e
rafalfalandys <rafalfalandys@gmail.com>
2023-08-18 18:23:07 +0200

    remove slide gesture and clear commented code

--- src/components/Modal/ImageBig.js
+++ src/components/Modal/ImageBig.js
@@ -1,38 +1,11 @@
-import { Fragment, useContext, useState } from "react";
+import { Fragment, useContext } from "react";
 import ReactPlayer from "react-player";
 import ContextProjects from "../../store/context-projects";
 
 import styles from "./ImageBig.module.scss";
 
 function ImageBig() {
-  const { curImages, curImg, curImgHandler } = useContext(ContextProjects);
-
-  //////////////////// slide gesture //////////////////////
-  const [touchStart, setTouchStart] = useState(null);
-  const [touchEnd, setTouchEnd] = useState(null);
-
-  // the required distance between touchStart and touchEnd to be detected as a swipe
-  const minSwipeDistance = 50;
-
-  const onTouchStart = (e) => {
-    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
-    setTouchStart(e.targetTouches[0].clientX);
-  };
-
-  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
-
-  const onTouchEnd = () => {
-    if (!touchStart || !touchEnd) return;
-    const distance = touchStart - touchEnd;
-    const isLeftSwipe = distance > minSwipeDistance;
-    const isRightSwipe = distance < -minSwipeDistance;
-    if (isLeftSwipe || isRightSwipe)
-      // add your conditional logic here
-      isLeftSwipe
-        ? curImgHandler(curImg === curImages.length - 1 ? 0 : curImg + 1)
-        : curImgHandler(curImg === 0 ? curImages.length - 1 : curImg - 1);
-  };
-  //////////////////// slide gesture //////////////////////
+  const { curImages, curImg } = useContext(ContextProjects);
 
   const url = curImages[curImg].url;
   const type = curImages[curImg].type;
@@ -40,12 +13,7 @@ function ImageBig() {
   return (
     <Fragment>
       {type !== "youtube" && (
-        <div
-          className={styles.container}
-          onTouchStart={onTouchStart}
-          onTouchMove={onTouchMove}
-          onTouchEnd={onTouchEnd}
-        >
+        <div className={styles.container}>
           {(!type || type === "img") && <img src={url} alt="big" />}
           {type === "video" && (
             <video src={url} alt="big" controls autoPlay playsinline />
@@ -53,20 +21,7 @@ function ImageBig() {
         </div>
       )}
       {type === "youtube" && (
-        <div
-          className={`${styles.container} ${styles["container--youtube"]}`}
-          onTouchStart={onTouchStart}
-          onTouchMove={onTouchMove}
-          onTouchEnd={onTouchEnd}
-        >
-          {/* <iframe
-            src={`https://youtube.com/embed/${curImages[curImg].url}?autoplay=1&mute=1`}
-            allow="autoplay"
-            title={curImages[curImg].name}
-            onTouchStart={onTouchStart}
-            onTouchMove={onTouchMove}
-            onTouchEnd={onTouchEnd}
-          ></iframe> */}
+        <div className={`${styles.container} ${styles["container--youtube"]}`}>
           <ReactPlayer
             url={`https://youtube.com/embed/${url}`}
             playing={true}
