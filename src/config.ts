892f7fa911108c9003a84febe09e220907fc1134
rafalfalandys <rafalfalandys@gmail.com>
2024-02-29 02:08:05 +0100

    Add fireabase config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // export const URL = `https://protected-chamber-04342-341ed7575df7.herokuapp.com/api/v1`;
 export const URL = `http://127.0.0.1:8000/api/v1`;
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA9dyaHoUFvHiLQXWvZ0UUhUQwe9yU-LGc",
-  authDomain: "weronika-kogut-aad.firebaseapp.com",
-  databaseURL: "https://weronika-kogut-aad-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "weronika-kogut-aad",
-  storageBucket: "weronika-kogut-aad.appspot.com",
-  messagingSenderId: "599510589800",
-  appId: "1:599510589800:web:890c76794561327f6e36dd",
+  apiKey: "AIzaSyAy3IZUME8Yw5wvq-A3A65TY5e_wCstGM0",
+  authDomain: "portfolio-f338a.firebaseapp.com",
+  databaseURL: "https://portfolio-f338a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "portfolio-f338a",
+  storageBucket: "portfolio-f338a.appspot.com",
+  messagingSenderId: "251998845714",
+  appId: "1:251998845714:web:d590c28d7ea5217bc951cb",
 };
